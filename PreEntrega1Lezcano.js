75a5b951190f627d30193639e067e977318f0045
Morena Lezcano <lezcanomorena03@gmail.com>
2024-09-19 12:57:45 -0300

    PreEntrega2

--- PreEntrega1Lezcano.js
+++ PreEntrega1Lezcano.js
@@ -1,7 +1,20 @@
 //Mi primer preentrega se va a tratar sobre una empresa que vende llaveros.//
 
-alert ("Bienvenido/a, nosotros somos 'Moly Cortelaser', nos especializamos en la venta de llaveros personalizados.");
+//Objetos
+class Llaveros {
+    constructor(material,valor,stok){
 
+        this.material=material;
+        this.valor=valor;
+        this.stok=stok;
+    }
+}
+
+const llaveros1=[
+    new Llaveros("acrilico",350,1000),
+    new Llaveros("madera",300,200),
+    new Llaveros("metal",500,800)
+]
 function calcularprecio(cantidad, precioUnidad){
     return precioUnidad * cantidad;
 }
@@ -25,9 +38,28 @@ function totalApagar(total){
     }
 }
 
+function validarMaterial(material){
+    return llaveros1.some( (el) => el.material.toLowerCase()=== material.toLowerCase())
+}
+
+function materialLlaveros(){
+    let materialLlavero= prompt("De que material quisieras tu/s llavero/s  \n*acrilico \n*madera \n*metal")
+
+    while(!validarMaterial(materialLlavero)){
+        alert("Ingresaste un material de llaveros invalido, porfavor vuelva a ingresar otro.")
+        materialLlavero= prompt("De que material quisieras tu/s llavero/s  \n*acrilico \n*madera \n*metal")
+
+    }
+    const llaveroEncontrado = llaveros1.find((el)=>el.material.toLowerCase()===materialLlavero.toLowerCase())
+
+    return{materialLlavero,llaveroEncontrado}
+
+    
+}
+
 function personalizado(){
-    const frace = prompt("¿Qué te gustaría que digan tus llaveros?");
-    const dije = prompt("¿Quisieras que tengan algún dije? (son de regalo) \n*si\n*no");
+    const frase = prompt("¿Qué te gustaría que digan tu/s llavero/s?");
+    const dije = prompt("¿Quisieras que tenga/n dije? (son de regalo) \n*si\n*no");
 
     let dijeSiNo;
     if (dije === 'si') {
@@ -36,44 +68,49 @@ function personalizado(){
         dijeSiNo = "Sin dije";
     }
 
-    return { frace, dijeSiNo };
+   return { frase, dijeSiNo};
 }
 
-function compra(){
-    const precioUnidad = 350;
+function findeCompra(cantidad){
+    const {materialLlavero,llaveroEncontrado}=materialLlaveros();
+    const personalizacion=personalizado();
 
-    for (let i=0;i< 1; i++) {
-        const opcion = parseInt(prompt("¿En qué podemos ayudarte?\n* Ingresa '1' si quieres un llavero.\n* Ingresa '2' si quieres dos.\n* Ingresa '3' si quieres más de dos llaveros.\n* Ingresa '4' si deseas salir."));
+    alert("* Cantidad de llaveros: " + cantidad + "\n* Dije/s: " + personalizacion.dijeSiNo + "\n* Frase: " + personalizacion.frase + "\n* Material: " + materialLlavero);
 
-        let cantidad = 0;
-        let frace = '';
-        let dijeSiNo = '';
-
-        if (opcion === 1) {
-            cantidad = 1;
-        } else if (opcion === 2) {
-            cantidad = 2;
-        } else if (opcion === 3) {
-            const opcion3 = parseInt(prompt("Ingrese el número de llaveros que quiere encargar. (Mayor a dos unidades)"));
-            cantidad = opcion3;
-        } else if (opcion === 4) {
-            break;
-        } else {
-            alert("El número que ingresaste no es válido. Por favor, ingresa un número válido '1', '2', '3' o '4'.");
-            continue;
-        }
-        
-        let total = calcularprecio(cantidad, precioUnidad);
+    let total = calcularprecio(cantidad, llaveroEncontrado.valor);
         total = descuento(total);
-
-        const { frace: fracePersonalizado, dijeSiNo: dijePersonalizado } = personalizado();
-        frace = fracePersonalizado;
-        dijeSiNo = dijePersonalizado;
-
-        alert("* Cantidad de llaveros: " + cantidad + "\n* Dije/s: " + dijeSiNo+ "\n* Frace: "+frace);
         totalApagar(total); 
-        alert("Gracias por confiar en nosotros!!! \n Moly Cortelaser.")
-    }
+    alert("Gracias por confiar en nosotros!!! \n Moly Cortelaser.")
 }
 
-compra();
+
+//Inicio del programa
+
+alert ("Bienvenido/a, nosotros somos 'Moly Cortelaser', nos especializamos en la venta de llaveros personalizados.");
+
+let opcion = parseInt(prompt("¿En qué podemos ayudarte?\n* Ingresa '1' si quieres un llavero.\n* Ingresa '2' si quieres dos.\n* Ingresa '3' si quieres más de dos llaveros."));
+if (opcion === 1 || opcion === 2 || opcion === 3 ){
+    let cantidad=0 
+    switch(opcion){
+        case 1:
+            cantidad=1
+            break; 
+        case 2:  
+            cantidad=2
+            break;
+        case 3:
+           let opcion3;
+            do {
+                opcion3 = parseInt(prompt("Ingrese el número de llaveros que quiere encargar. (Mayor a dos unidades)"));
+                if (opcion3 <= 2) {
+                    alert("Por favor, ingrese un número mayor a 2.");
+                }
+            } while (opcion3 <= 2);
+            cantidad = opcion3;
+            break;
+    }  
+    findeCompra(cantidad);
+}else{
+    alert("El numero que ingresaste es incorrecto, por favor ingrese 1, 2 o 3")
+  
+}
